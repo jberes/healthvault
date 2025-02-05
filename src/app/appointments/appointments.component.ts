4c2ab1e35082567b16b38dcfc9a0e06bec70683d
Jason Beres <jasonberes@gmail.com>
2025-02-05 13:39:26 -0500

    Health Vault initial version

---
+++ src/app/appointments/appointments.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-appointments',
+  standalone: false,
+  templateUrl: './appointments.component.html',
+  styleUrls: ['./appointments.component.scss']
+})
+export class AppointmentsComponent {
+}
---
+++ src/app/medications/medications.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-medications',
+  standalone: false,
+  templateUrl: './medications.component.html',
+  styleUrls: ['./medications.component.scss']
+})
+export class MedicationsComponent {
+}
